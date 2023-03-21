995d40f7a57a5c298afd3377aac368ae5e1364c2
Ashok <dhakalashok95@gmail.com>
2023-03-21 21:30:05 +0545

    Learn Ado.Net

--- Web Develpoment/CollegeManagement/CollegeManagement.Web/Controllers/HomeController.cs
+++ Web Develpoment/CollegeManagement/CollegeManagement.Web/Controllers/HomeController.cs
@@ -1,32 +1,95 @@
-﻿using CollegeManagement.Web.Models;
+﻿//using CollegeManagement.Web.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Data.SqlClient;
+//using System.Diagnostics;
+
+//namespace CollegeManagement.Web.Controllers
+//{
+//    public class HomeController : Controller
+//    {
+//        public IActionResult Index() 
+//        {
+//        return View();
+//        }
+
+//        public IActionResult LearnAdoNET()
+//        {
+//            //Fetch student data from student table 
+//            string connectionString = " string connectionString = \"Data Source=(localdb)\\\\MSSQLLocalDB;Initial Catalog=CollegeDb_test;Integrated Security=True;"
+//                        SqlConnection connection = new SqlConnection(connectionString);
+//            SqlCommand command = new SqlCommand("Select * from dbo.Student", connection);
+
+//            connection.Open();
+//            SqlDataReader reader = command.ExecuteReader();
+
+//            List<Student> students = new List<Student>();
+//            while (reader.Read())
+//            {
+//                var student = new Student()
+//                {
+//                    RollNo = reader.GetInt32(0),
+//                    Name = reader.GetString(1),
+//                    Contact = reader.GetString(2),
+//                    Major = reader.GetString(3),
+//                    Semester = reader.GetString(5),
+//                };
+//                students.Add(student);
+//            }
+//              return View("ADO",students);
+//        }
+//    }
+//    public IActionResult Privacy()
+//    {
+//        return view();
+//    }
+//}
+using CollegeManagement.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using System.Diagnostics;
 
 namespace CollegeManagement.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult LearnAdoNET()
+        {
+            //Fetch student data from student table 
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CollegeDb_test;Integrated Security=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand("Select * from dbo.Student", connection);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Student> students = new List<Student>();
+            while (reader.Read())
+            {
+                var student = new Student()
+                {
+                    RollNo = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Contact = reader.GetString(2),
+                    Major = reader.GetString(3),
+                    Semester = reader.GetString(5),
+                };
+                students.Add(student);
+            }
+            return View("ADO", students);
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
+
+
+
+
