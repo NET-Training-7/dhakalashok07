619b1c0098648bfde84a45469cc29c6fb28b83b2
Ashok <dhakalashok95@gmail.com>
2023-03-25 15:39:19 +0545

    Student Registration

--- Web Develpoment/CollegeManagement/CollegeManagement.Web/Controllers/StudentsController.cs
+++ Web Develpoment/CollegeManagement/CollegeManagement.Web/Controllers/StudentsController.cs
@@ -2,10 +2,10 @@
 using CollegeManagement.Web.Models;
 using CollegeManagement.Web.Data;
 //using System.Reflection.Metadata.Ecma335;
-
 namespace CollegeManagement.Web.Controllers;
 public class StudentsController : Controller
 {
+    CollegeDbConext db = new CollegeDbConext();
     //List<Student> students;
     //public StudentsController()
     //{
@@ -21,23 +21,42 @@ public class StudentsController : Controller
     //{
     //    return View(students);
     //}
-//{   
-//        CollegeDbConext db = new CollegeDbConext();
-//    var students = db.Students.ToList();
-//        return View(students);
-//}
+    //{   
+    //        CollegeDbConext db = new CollegeDbConext();
+    //    var students = db.Students.ToList();
+    //        return View(students);
+    //}
 
 
-public IActionResult Index()
-{
-    CollegeDbConext db = new CollegeDbConext();
-    var students = db.Students.ToList();
-    return View(students);
-}
+    public IActionResult Index()
+    {
+        CollegeDbConext db = new CollegeDbConext();
+        var students = db.Students.ToList();
+        return View(students);
+    }
+    public IActionResult Details(int id)
+    {
+        var student = db.Students.Find(id);// select * from Students where Id = id
+        return View(student);
+    }
+
+    public IActionResult Add()
+    {
+        return View();
+    }
 
     //public IActionResult Details(int id)
     //{
     //    var student = students.Where(x => x.RollNo == id).FirstOrDefault();
     //    return View(student);
     //}
+    [HttpPost]
+    public IActionResult Add(Student student)
+    {
+        db.Students.Add(student);
+        db.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
 }
+
--- Web Develpoment/CollegeManagement/CollegeManagement.Web/Models/Students.cs
+++ Web Develpoment/CollegeManagement/CollegeManagement.Web/Models/Students.cs
@@ -1,19 +1,43 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
 
 namespace CollegeManagement.Web.Models;
 public class Student
 {
-    [Key]
     public int Id { get; set; }
-    public int RollNo { get; set; }
-    public string? Name { get; set; } /*=string.Empty;*/ //public pachadi ? or = string.Empty any one will work
 
-    public string?Address { get; set; }
+    [DisplayName("Roll Number")]
+    public int RollNo { get; set; }
+
+    public string? Name { get; set; }
+    public string? Address { get; set; }
     public string? Gender { get; set; }
+
+    [DisplayName("Date Of Birth")]
     public DateTime? Dob { get; set; }
+
     public string? Contact { get; set; }
     public string? Email { get; set; }
     public string? Major { get; set; }
     public string? Semester { get; set; }
-}
 
+}
+//using System.ComponentModel.DataAnnotations;
+
+//namespace CollegeManagement.Web.Models;
+//public class Student
+//{
+//    [Key]
+//    public int Id { get; set; }
+//    public int RollNo { get; set; }
+//    public string? Name { get; set; }
+
+//    public string?Address { get; set; }
+//    public string? Gender { get; set; }
+//    public DateTime? Dob { get; set; }
+//    public string? Contact { get; set; }
+//    public string? Email { get; set; }
+//    public string? Major { get; set; }
+//    public string? Semester { get; set; }
+//}
+
+///*=string.Empty;*/ //public pachadi ? or = string.Empty any one will work
